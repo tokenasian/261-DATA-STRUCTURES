9e9fc7f96700ffa10045ccee21775a6dc7c601b2
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-05-02 11:04:31 -0400

    Update sll.py

--- sll.py
+++ sll.py
@@ -1,11 +1,12 @@
-# Name:
-# OSU Email:
+# Name: Matthew Armstrong
+# OSU Email: armstrm2@oregonstate.edu
 # Course: CS261 - Data Structures
-# Assignment:
-# Due Date:
-# Description:
+# Assignment: Assignment 3
+# Due Date: May 2nd
+# Description: Implement a Singly LinkedList data structure.
 
 
+from ast import Return
 from SLNode import *
 
 
@@ -69,66 +70,131 @@ class LinkedList:
 
     def insert_front(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        Adds a new node at the beginning of the list.
         """
-        node = SLNode(value)
-        node.next = self._head.next
-        self._head.next = node
+
+        new_node = SLNode(value)
+        new_node.next = self._head.next
+        self._head.next = new_node
 
     def insert_back(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        Adds a new node at the end of the list.
         """
-        pass
+        new_node = SLNode(value)
+        if self._head is None:
+            self._head = new_node
+            return
+        
+        end = self._head
+        while (end.next) is not None:
+            end = end.next
+        end.next = new_node
 
     def insert_at_index(self, index: int, value: object) -> None:
         """
-        TODO: Write this implementation
+        Inserts a new value at the specified index position in the linked list.
+        Index 0 refers to the beginning of the list (right after the front sentinel).
         """
-        if index < 0 or index > self.length():
-            raise SLLException
+        if index < 0 or index > self.length():  # is the index out of range?
+            raise SLLException  # if so, raise exception
 
-        prev_node = self._head
+        count = 0  # initialize count
+        curr = self._head  # initialize pointer
 
-        while index != 0:
-            prev_node = prev_node.next
-            index -= 1
+        while count != index:  # traverse through the list, locate the specified index position 
+            curr = curr.next
+            count += 1  # increment the count by 1
 
-        node = SLNode(value)
-        node.next = prev_node.next
-        prev_node.next = node
-
+        node = SLNode(value)  # insert the new node
+        node.next = curr.next  # update the pointers
+        curr.next = node
 
     def remove_at_index(self, index: int) -> None:
         """
-        TODO: Write this implementation
+        Removes the node at the specified index position from the linked list.
+        Index 0 refers to the beginning of the list (right after the front sentinel).
         """
-        pass
+        if index < 0 or index >= self.length():  # is the index out of range?
+            raise SLLException  # if so, raise exception
+        
+        # initialize the count
+        count = 0
+        current = self._head  
+        
+        while count != index:  # traverse through the list, locate the target node at the given index
+            current = current.next
+            count += 1  # increment the count by 1
+
+
+        temp = current.next.next  # update the pointers
+        current.next = temp  
 
     def remove(self, value: object) -> bool:
         """
-        TODO: Write this implementation
+        Traverses the list from the beginning to the end
+        Removes the first node that matches the provided “value” object. 
         """
-        pass
+        node = self._head 
+        while node.next is not None:  # traverse through the list, find the node that matches the passed object
+            if value == node.next.value:  # remove the first node matching provided “value” object
+                node.next = node.next.next
+                return True 
+            else: 
+                node = node.next
+        return False  # no matching node value found upon reaching the tail
 
     def count(self, value: object) -> int:
         """
-        TODO: Write this implementation
+        Counts the number of elements in the list that match the provided “value” object. 
+        The method then returns this number.
         """
-        pass
+        count = 0  # initialize the count
+        current = self._head.next
+
+        while current is not None:  # traverse through the list until we reach the end
+            if value == current.value:  # locate the matching target value
+                count += 1  # increment the count by 1
+            current = current.next
+        return count  # return the count 
 
     def find(self, value: object) -> bool:
         """
-        TODO: Write this implementation
+        Returns a Boolean value based on whether or not the provided “value” object exists in the list.
         """
-        pass
+        current = self._head
+        while current.next is not None:
+            if value == current.next.value:
+                return True
+            current = current.next
+        return False
 
     def slice(self, start_index: int, size: int) -> "LinkedList":
         """
-        TODO: Write this implementation
+        Returns a new LinkedList object that contains,
+        the requested number of nodes from the original list, 
+        starting with the node located at the requested start index. 
+        If the original list contains N nodes, 
+        a valid start_index is in range [0, N - 1] inclusive. 
+        The original list cannot be modified. 
+        The runtime complexity of your implementation must be O(N).
         """
-        pass
+        if start_index < 0 or start_index >= self.length() or start_index + size > self.length() or size < 0:
+            raise SLLException
 
+        current = self._head
+
+        while start_index != 0:
+            current = current.next
+            start_index -= 1
+
+        newLL = LinkedList()
+
+        while size > 0:
+            current = current.next
+            newLL.insert_back(current.value)
+            size -= 1
+        return newLL
 
 if __name__ == '__main__':
 
